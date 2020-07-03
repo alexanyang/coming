8844c60b68dc1b2f39029b06fac63753461b891c
anyang <alexanyang@foxmail.com>
2020-07-03 10:18:37 +0800

    原始的分支

--- schedule_task.go
+++ schedule_task.go
@@ -1,7 +1,6 @@
 package coming
 
 import (
-	"sync"
 	"time"
 )
 
@@ -13,44 +12,33 @@ type schedule struct {
 
 const ANY = -1 // mod by MDR
 
-var (
-	tasks  map[string]schedule
-	locker sync.RWMutex
-)
+var tasks []schedule
 
 // 这个function新建一个简单定时任务到后台任务集,schedule可以通过month, day, weekday, hour, minute, second.
 // 这6个字段匹配到一年中的任意时刻,最小精度为秒
 // this function create a new scheduled task and append to tasks,whitch will be checked every second.
 // this schedule task can match any time in a year with minimum precision second
-func NewSchedule(month, day, weekday, hour, minute, second int8, name string, task func(time.Time)) {
+func NewSchedule(month, day, weekday, hour, minute, second int8, task func(time.Time)) {
 	cj := schedule{month, day, weekday, hour, minute, second, task}
-	locker.Lock()
-	defer locker.Unlock()
-	tasks[name] = cj
-}
-
-func RemoveSchedule(name string) {
-	locker.Lock()
-	defer locker.Unlock()
-	delete(tasks, name)
+	tasks = append(tasks, cj)
 }
 
 // 创建一个定时任务,匹配一个月中的任意时间,但时间必须是明确的
 // this creates a new scheduled task matched any exactly time in a month.
-func NewMonthlySchedule(day, hour, minute, second int8, name string, task func(time.Time)) {
-	NewSchedule(ANY, day, ANY, hour, minute, second, name, task)
+func NewMonthlySchedule(day, hour, minute, second int8, task func(time.Time)) {
+	NewSchedule(ANY, day, ANY, hour, minute, second, task)
 }
 
 // 创建一个定时任务,匹配一周中的任意时间,但时间必须是明确的
 // this creates a new scheduled task matched any exactly time in a week.
-func NewWeeklySchedule(weekday, hour, minute, second int8, name string, task func(time.Time)) {
-	NewSchedule(ANY, ANY, weekday, hour, minute, second, name, task)
+func NewWeeklySchedule(weekday, hour, minute, second int8, task func(time.Time)) {
+	NewSchedule(ANY, ANY, weekday, hour, minute, second, task)
 }
 
 // 创建一个定时任务,匹配一天中的任意时间,但时间必须是明确的
 // this creates a new scheduled task matched any exactly time of day.
-func NewDailySchedule(hour, minute, second int8, name string, task func(time.Time)) {
-	NewSchedule(ANY, ANY, ANY, hour, minute, second, name, task)
+func NewDailySchedule(hour, minute, second int8, task func(time.Time)) {
+	NewSchedule(ANY, ANY, ANY, hour, minute, second, task)
 }
 
 func (sc schedule) Matches(t time.Time) (ok bool) {
@@ -66,19 +54,16 @@ func (sc schedule) Matches(t time.Time) (ok bool) {
 func processSchedules() {
 	for {
 		now := time.Now()
-		locker.RLock()
 		for _, j := range tasks {
 			// execute all our cron tasks asynchronously
 			if j.Matches(now) {
 				go j.Task(now)
 			}
 		}
-		locker.RUnlock()
 		time.Sleep(time.Second)
 	}
 }
 
 func init() {
-	tasks = make(map[string]schedule)
 	go processSchedules()
 }
